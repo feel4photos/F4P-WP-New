f57a42e8eec160290e0aaa35869ad422c7c59165
feel4photos <Samhita-Sudipta@Macbooks-MacBook-Pro.local>
2018-01-27 22:11:45 +0530

    page excerpt plugin added and about fragment is dynamic in front page.

--- wp-content/themes/feel4photos/front-page.php
+++ wp-content/themes/feel4photos/front-page.php
@@ -14,42 +14,8 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-		front page
 
-		<?php // Show the selected frontpage content.
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
-			endwhile;
-		else :
-			get_template_part( 'template-parts/post/content', 'none' );
-		endif; ?>
+	<?php get_template_part ( 'template-parts/page/about', 'fragment' ); ?><!-- / about fragment partial -->
 
-		<?php
-		// Get each of our panels and show the post data.
-		if ( 0 !== feel4photos_panel_count() || is_customize_preview() ) : // If we have pages to show.
-
-			/**
-			 * Filter number of front page sections in Twenty Seventeen.
-			 *
-			 * @since Twenty Seventeen 1.0
-			 *
-			 * @param int $num_sections Number of front page sections.
-			 */
-			$num_sections = apply_filters( 'feel4photos_front_page_sections', 4 );
-			global $feel4photoscounter;
-
-			// Create a setting and control for each of the sections available in the theme.
-			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-				$feel4photoscounter = $i;
-				feel4photos_front_page_section( null, $i );
-			}
-
-	endif; // The if ( 0 !== feel4photos_panel_count() ) ends here. ?>
-
-	</main><!-- #main -->
-</div><!-- #primary -->
 
 <?php get_footer();
---
+++ wp-content/themes/feel4photos/template-parts/page/about-fragment.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Feel4_Photos
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+<section class="o-hlight c-about u-height100 u-height-inherit wow fadeInUp">
+    <section class="container c-about__inner u-height100-prcnt">
+        <div class="row u-height100-prcnt">
+            <div class="col s12 l5 valign-wrapper o-white u-height100-prcnt hide-on-med-and-down c-equalhght">
+                <div class="u-allpad-50 u-margin-0auto">
+                    <img src="images/logo-black.png" alt="Feel4photos" class="c-about__logo responsive-img">
+                </div>
+            </div><!-- / large logo about us are -->
+            <div class="col s12 l7 u-height100-prcnt c-equalhght">
+            	<?php $pagedata=get_page(7); ?>
+                <div class="card white-text u-topbottom-pad-50 u-left-pad u-right-pad c-about__hcontent">
+					<div class="card-content u-width80">
+                        <?php echo '<h2 class="card-title c-about__title c-title c-title--border-bottom c-title--border-base-color u-uppercase u-no-topmargin">'.$pagedata->post_title.'</h2>'; ?>
+                        <div class="c-about__desc">
+                            <?php echo wpautop($pagedata->post_excerpt); ?>
+                        </div>
+                    </div>
+                    <div class="card-action">
+                        <a href="<?php echo get_permalink(7); ?>" title="More" class="c-about__mlink">More</a>
+                    </div>
+                </div>
+                <?php ?>
+            </div>
+        </div>
+    </section><!-- / gm fixed width container -->
+</section><!-- / about -->
