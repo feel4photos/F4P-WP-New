a138f26c4955d7bea1cae745f9e42b4572677da9
feel4photos <Samhita-Sudipta@Macbooks-MacBook-Pro.local>
2018-05-16 22:01:55 +0530

    add pinterest icon to social platform.

--- wp-content/themes/feel4photos/template-parts/common/social-platforms.php
+++ wp-content/themes/feel4photos/template-parts/common/social-platforms.php
@@ -13,4 +13,5 @@
 <a href="<?php echo get_option('facebook_link');?>" target="_blank" class="u-linkbase social facebook"><i class="fa fa-facebook"></i></a>
 <!-- <a href="javascript:void(0);" class="u-linkbase social twitter"><i class="fa fa-twitter"></i></a> -->
 <a href="<?php echo get_option('instagram_link');?>" target="_blank" class="u-linkbase social instagram"><i class="fa fa-instagram"></i></a>
+<a href="javascript:void(0);" class="u-linkbase social pinterest"><i class="fa fa-pinterest"></i></a>
 
