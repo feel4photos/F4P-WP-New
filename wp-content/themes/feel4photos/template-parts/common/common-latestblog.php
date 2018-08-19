8a43396e76653151d12a9c93cb19da3c7befcb5d
feel4photos <Samhita-Sudipta@Samhita-and-Sudipta.local>
2018-08-19 22:46:36 +0530

    footer latest blog section is on progress.

---
+++ wp-content/themes/feel4photos/template-parts/common/common-latestblog.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Displays latest blog credentials of Feel4photos
+ *
+ * @package WordPress
+ * @subpackage Feel4_Photos
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+<section class="o-overlay--05 c-hblog u-topbottom-pad-50 white-text parallax-container wow fadeInUp">
+    <section class="container u-left-pad u-right-pad">
+        <div class="row">
+            <div class="col s12 l8">
+                <div class="card">
+                    <div class="card-content u-width80">
+                        <h2 class="card-title c-title c-title--border-bottom c-title--border-hlight-color c-hblog__title u-uppercase u-no-topmargin">
+                            <span class="u-capitalize u-smallertxt u-bauhaus-ffamily">Our Fashion</span>
+                            Concepts
+                        </h2>
+                        <div class="c-hblog__desc">
+                            <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+                        </div>
+                    </div>
+                    <div class="card-action">
+                        <a href="javascript:void(0);" title="More" class="c-hblog__mlink">More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 l4"></div>
+        </div>
+    </section><!-- / gm fixed width container -->
+    <section class="c-hblog__featureimg parallax">
+        <img src="images/blog-bg2.jpg" alt="">
+    </section><!-- / feature image holder -->
+</section><!-- / blog in home page -->
