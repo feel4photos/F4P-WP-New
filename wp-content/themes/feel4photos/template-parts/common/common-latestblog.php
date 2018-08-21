9505c8bcdfab6959ecab1cc7ef3cc01c9403270e
feel4photos <Samhita-Sudipta@Samhita-and-Sudipta.local>
2018-08-21 22:38:42 +0530

    latest blog section is on progress.

--- wp-content/themes/feel4photos/template-parts/common/common-latestblog.php
+++ wp-content/themes/feel4photos/template-parts/common/common-latestblog.php
@@ -10,22 +10,36 @@
 
 ?>
 
-<section class="o-overlay--05 c-hblog u-topbottom-pad-50 white-text parallax-container wow fadeInUp">
+<section class="u-overlay--black-reverse c-hblog u-topbottom-pad-50 white-text parallax-container wow fadeInUp">
+    <?php
+        //$pagedlatestpost = (get_query_var('paged')) ? get_query_var('paged') : 1;
+        $latestBlogArgs = array(
+            'order' => 'desc',
+            'orderby' => 'post_date',
+            'posts_per_page' => 1,
+            'post_type' => 'post',
+            'post_status' => 'publish'
+            //'paged' => $pagedlatestpost
+        );
+        $latestPosts = new WP_Query( $latestBlogArgs );
+        while ( $latestPosts->have_posts() ) : $latestPosts->the_post();
+        //$portfolioThumb = get_the_post_thumbnail_url(get_the_ID(),'full');
+    ?>
     <section class="container u-left-pad u-right-pad">
         <div class="row">
             <div class="col s12 l8">
                 <div class="card">
-                    <div class="card-content u-width80">
+                    <div class="card-content u-width75">
                         <h2 class="card-title c-title c-title--border-bottom c-title--border-hlight-color c-hblog__title u-uppercase u-no-topmargin">
                             <span class="u-capitalize u-smallertxt u-bauhaus-ffamily">Our Fashion</span>
-                            Concepts
+                            <?php the_title(); ?>
                         </h2>
                         <div class="c-hblog__desc">
-                            <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+                            <?php the_content(); ?>
                         </div>
                     </div>
                     <div class="card-action">
-                        <a href="javascript:void(0);" title="More" class="c-hblog__mlink">More</a>
+                        <a href="<?php the_permalink(); ?>" title="More" class="c-hblog__mlink">More</a>
                     </div>
                 </div>
             </div>
@@ -33,6 +47,7 @@
         </div>
     </section><!-- / gm fixed width container -->
     <section class="c-hblog__featureimg parallax">
-        <img src="images/blog-bg2.jpg" alt="">
+        <?php the_post_thumbnail('full'); ?>
     </section><!-- / feature image holder -->
+    <?php endwhile; wp_reset_query(); ?>
 </section><!-- / blog in home page -->
