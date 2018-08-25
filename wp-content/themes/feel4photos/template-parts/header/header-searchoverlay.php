817b6ad566a2076265559032153fa92d0746d5aa
feel4photos <Samhita-Sudipta@Samhita-and-Sudipta.local>
2018-08-25 16:18:35 +0530

    updated small modifications.

--- wp-content/themes/feel4photos/template-parts/header/header-searchoverlay.php
+++ wp-content/themes/feel4photos/template-parts/header/header-searchoverlay.php
@@ -27,9 +27,14 @@
                     <?php echo _x( 'Search for:', 'label', 'feel4photos' ); ?>
                 </span>
             </label>
-            <input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'feel4photos' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+            <input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder', 'feel4photos' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
             <button type="submit" class="search-submit">
-                <?php echo feel4photos_get_svg( array( 'icon' => 'search' ) ); ?>
+                <?php //echo feel4photos_get_svg( array( 'icon' => 'search' ) ); ?>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 512 512" enable-background="new 0 0 512 512" width="25px" height="25px">
+                  <g>
+                    <path d="M495,466.2L377.2,348.4c29.2-35.6,46.8-81.2,46.8-130.9C424,103.5,331.5,11,217.5,11C103.4,11,11,103.5,11,217.5   S103.4,424,217.5,424c49.7,0,95.2-17.5,130.8-46.7L466.1,495c8,8,20.9,8,28.9,0C503,487.1,503,474.1,495,466.2z M217.5,382.9   C126.2,382.9,52,308.7,52,217.5S126.2,52,217.5,52C308.7,52,383,126.3,383,217.5S308.7,382.9,217.5,382.9z" fill="#FFFFFF"/>
+                  </g>
+                </svg>
                 <span class="screen-reader-text">
                     <?php echo _x( 'Search', 'submit button', 'feel4photos' ); ?>
                 </span>
